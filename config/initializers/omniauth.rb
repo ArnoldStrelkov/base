f55148595a9a5fca79046b93c255bd9b76ec2704
ArnoldStrelkov <wikiuspeha@gmail.com>
2016-02-23 12:23:48 +0000

    env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1112623815449018', '1e4bc460c4491829b57c5084ee450c59'
-  provider :vkontakte, '5232202', 'NYou4aIRmkywh2cGY8lq', {
+ # provider :vkontakte, '5232202', 'NYou4aIRmkywh2cGY8lq', { :image_size => 'bigger_x2'}
       
-      :image_size => 'bigger_x2'
-    }
+     provider :vkontakte,  ENV['VKID'],  ENV['VKPASS'], { :image_size => 'bigger_x2'}
+    
 end
